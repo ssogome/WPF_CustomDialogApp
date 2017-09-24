819989c5e72b943f99637ee17d76b53ca5ad37b3
Pap Sogome <ssogome@gmail.com>
2017-09-24 17:06:07 -0600

    Add project files.

---
+++ WPF_CustomDialogApp/TestCustomInputWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_CustomDialogApp
+{
+    /// <summary>
+    /// Interaction logic for TestCustomInputWindow.xaml
+    /// </summary>
+    public partial class TestCustomInputWindow : Window
+    {
+        //public TestCustomInputWindow()
+        //{
+        //    InitializeComponent();
+        //}
+
+        public TestCustomInputWindow(string question, string defaultAnswer = "")
+        {
+            InitializeComponent();
+            lblQuestion.Content = question;
+            txtAnswer.Text = defaultAnswer;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            txtAnswer.SelectAll();
+            txtAnswer.Focus();
+        }
+
+        public string Answer
+        {
+            get { return txtAnswer.Text; }
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
